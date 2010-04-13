cd882685b03ead65c969b0db768c9d750dbb0512
Victor Savkin <avix1000@gmail.com>
2010-04-13 22:29:41 +1100

    for debug

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -39,6 +39,7 @@ class WordsController < ApplicationController
     description = @facade.describe(str: params[:str])
     render text: description
   rescue Exception => e
+    puts e.inspect
     render nothing: true, status: 404
   end
 end
