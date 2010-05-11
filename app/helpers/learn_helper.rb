0fe8abe85824be3d1dde21bc6e6c2a9096b87309
Victor Savkin <avix1000@gmail.com>
2010-05-11 17:57:58 +1100

    improve: formatter of the learning page

--- app/helpers/learn_helper.rb
+++ app/helpers/learn_helper.rb
@@ -1,2 +1,15 @@
 module LearnHelper
+  def format_explanation(word, text)
+    text.gsub!(/\[\w+\]/){|e| "<span class='wordType'>#{e}</span>"}
+
+    lines = text.split("\n").collect{|line| line.strip}
+    lines = lines.collect do |line|
+      if line[0] == '+'
+        "<span class='wordExample'>#{line}</span>"
+      else
+        line  
+      end
+    end
+    lines[0..12].join("<br>")
+  end
 end
