2be378097dab4c9c3c635c3bef7f7b32420bdb0e
Victor Savkin <avix1000@gmail.com>
2010-05-14 17:36:46 +1100

    better formatting

--- app/helpers/learn_helper.rb
+++ app/helpers/learn_helper.rb
@@ -1,15 +1,31 @@
 module LearnHelper
   def format_explanation(word, text)
-    text.gsub!(/\[\w+\]/){|e| "<span class='wordType'>#{e}</span>"}
+    text = wrap_word_note_parts(text)
+    lines = lines(text)
+    lines = wrap_example_lines(lines)
+    lines[0...12].join("<br>")
+  end
 
-    lines = text.split("\n").collect{|line| line.strip}
-    lines = lines.collect do |line|
-      if line[0] == '+'
+  private
+  def wrap_word_note_parts(text)
+    text.gsub(/\[\w+\]/) { |e| "<span class='wordNote'>#{e}</span>" }
+  end
+
+  def wrap_example_lines(lines)
+    lines.collect do |line|
+      if line_with_example? line
         "<span class='wordExample'>#{line}</span>"
       else
-        line  
+        line
       end
     end
-    lines[0..12].join("<br>")
+  end
+
+  def lines(text)
+    text.split("\n").collect { |line| line.strip }
+  end
+
+  def line_with_example?(line)
+    line[0] == '+'
   end
 end
--- spec/helpers/learn_helper_spec.rb
+++ spec/helpers/learn_helper_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 
 describe LearnHelper do
 
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(LearnHelper)
+  it "should wrap lines with examples" do
+    formatted = helper.format_explanation('', "one\n+two")
+    formatted.should == "one<br><span class='wordExample'>+two</span>"
+  end
+
+  it "should wrap special sequences" do
+    formatted = helper.format_explanation('', "[countable]one")
+    formatted.should == "<span class='wordType'>[countable]</span>one"
+  end
+
+  it "should show only first 12 lines" do
+    formatted = helper.format_explanation('', (1..13).to_a.join("\n"))
+    formatted.should == (1..12).to_a.join("<br>")
   end
 
 end
